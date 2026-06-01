448584a0c16295f2dc8fbe20fdd364bc7374f4ab
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-01 17:49:01 +0530

    Update Q3.java

--- DAY1/Q3.java
+++ DAY1/Q3.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class Q3 {
+    public static void main(String[] args) {
 
+        int n = 0;
+
+        System.out.println("Q3) Write a program to find factorial of a number.");
+        System.out.print("Enter N: ");
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        int ans = 1;
+
+        while (n > 0) {
+            ans = ans * n;
+            n--;
+        }
+
+        System.out.println("Factorial: " + ans);
+
+        sc.close();
+    }
+}
