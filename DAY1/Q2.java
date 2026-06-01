3c4860b0e172b0acdece97b8afcf720e25ca93db
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-01 17:47:55 +0530

    Update Q2.java

--- DAY1/Q2.java
+++ DAY1/Q2.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Q2 {
+    public static void main(String[] args) {
+        int n;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Q2) Write a program to print multiplication table of a given number.");
+        System.out.print("Enter N: ");
+        n = sc.nextInt();
+
+        int i = 1;
+
+        while (i <= 10) {
+            int ans = n * i;
+            System.out.println(n + " x " + i + " = " + ans);
+            i++;
+        }
+
+        sc.close();
+    }
+}
