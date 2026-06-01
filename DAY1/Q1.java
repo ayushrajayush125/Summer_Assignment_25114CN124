199889d83a097b4348ea965c9e514fc0dfe5243c
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-01 17:46:38 +0530

    Update Q1.java

--- DAY1/Q1.java
+++ DAY1/Q1.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Q1 {
+    public static void main(String[] args) {
+
+        int n = 0;
+
+        System.out.println("Q1) Write a program to calculate sum of first N natural numbers.");
+        System.out.print("Enter N: ");
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        int i = 1;
+        int sum = 0;
+
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("ANS: " + sum);
+
+        sc.close();
+    }
+}
