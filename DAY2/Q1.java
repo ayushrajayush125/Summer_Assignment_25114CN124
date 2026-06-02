9c1dc0ded55055e4b94a79755eefee2f8364e163
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-02 12:39:13 +0530

    Update Q1.java

--- DAY2/Q1.java
+++ DAY2/Q1.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Q5 {
+    public static void main(String[] args) {
+        int n = 0;
+
+        System.out.println("Q5) Write a program to calculate sum of digits of a number.");
+        System.out.print("Enter N: ");
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        int sum = 0;
+
+        while (n > 0) {
+            int digi = n % 10;
+            sum += digi;
+            n /= 10;
+        }
+
+        System.out.println("ANS: " + sum);
+
+        sc.close();
+    }
+}
