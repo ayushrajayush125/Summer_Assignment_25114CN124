b2842ea6f9bdfc0b9b0ac4e9b8e45226de6443f9
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-02 19:19:44 +0530

    Update Q3.java

--- DAY2/Q3.java
+++ DAY2/Q3.java
@@ -1,1 +1,21 @@
-
+package day2;
+import java.util.Scanner;
+public class Q3{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("\nQ7) Write a program to find product of digits.");
+        System.out.print("Enter N: ");
+        n=sc.nextInt();
+        int ans=1;
+        if(n<0)
+            n=-n;
+        while(n>0){
+            int digit=n%10;
+            ans=ans*digit;
+            n/=10;
+        }
+        System.out.println("ANS: "+ans);
+        sc.close();
+    }
+}
