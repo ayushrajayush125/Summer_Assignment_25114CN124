d2b752c679204dbb861a70f0842c26f10d658b2d
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-02 19:10:19 +0530

    Update Q2.java

--- DAY2/Q2.java
+++ DAY2/Q2.java
@@ -1,1 +1,32 @@
-
+package day2;
+import java.util.Scanner;
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Q2) Write a program to reverse a number.");
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        int ans = 0;
+        boolean isNegative = false;
+        if (n < 0) {
+            isNegative = true;
+            n = -n;
+        }
+        while (n > 0) {
+            int digit = n % 10;
+            if (ans > Integer.MAX_VALUE / 10 ||
+                (ans == Integer.MAX_VALUE / 10 && digit > 7)) {
+                System.out.println("invalid");
+                sc.close();
+                return;
+            }
+            ans = ans * 10 + digit;
+            n /= 10;
+        }
+        if (isNegative) {
+            ans = -ans;
+        }
+        System.out.println("ANS: " + ans);
+        sc.close();
+    }
+}
