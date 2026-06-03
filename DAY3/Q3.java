9c9be6690d0926606d91e974247cdfcd1509890a
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-03 18:10:51 +0530

    Update Q3.java

--- DAY3/Q3.java
+++ DAY3/Q3.java
@@ -1,1 +1,25 @@
-
+import java.util.Scanner;
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write a program to Find GCD of two numbers.");
+        int divisor = 0;
+        int dividend = 0;
+        System.out.print("Enter first number: ");
+        divisor = sc.nextInt();
+        System.out.print("Enter second number: ");
+        dividend = sc.nextInt();
+        if (dividend < divisor) {
+            int temp = dividend;
+            dividend = divisor;
+            divisor = temp;
+        }
+        while (divisor != 0) {
+            int remainder = dividend % divisor;
+            dividend = divisor;
+            divisor = remainder;
+        }
+        System.out.println(dividend);
+        sc.close();
+    }
+}
