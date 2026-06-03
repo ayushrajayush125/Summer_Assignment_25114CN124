847ae0626814df41a5933615ec66b72a27c545e0
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-03 18:12:38 +0530

    Update Q4.java

--- DAY3/Q4.java
+++ DAY3/Q4.java
@@ -1,1 +1,27 @@
-
+import java.util.Scanner;
+public class Q4{
+    static int getGCD(int divisor,int dividend){
+        if(dividend<divisor){
+            int temp=dividend;
+            dividend=divisor;
+            divisor=temp;
+        }
+        while(divisor!=0){
+            int remainder=dividend%divisor;
+            dividend=divisor;
+            divisor=remainder;
+        }
+        return dividend;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Write a program to Find LCM of two numbers.");
+        System.out.print("Enter first number: ");
+        int first=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int second=sc.nextInt();
+        int ans=(first*second)/getGCD(first,second);
+        System.out.println(ans);
+        sc.close();
+    }
+}
