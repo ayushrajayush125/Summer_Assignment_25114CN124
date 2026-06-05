7eff49eba75543523c61285f525c70df93d0089e
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-05 08:56:10 +0530

    Update Q1.java

--- DAY4/Q1.java
+++ DAY4/Q1.java
@@ -1,1 +1,27 @@
-and
+import java.util.*;
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("Write a program to generate Fibonacci series.");
+
+        int prev = 0;
+        int next = 1;
+        int n = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i == 1) {
+                System.out.println(i);
+            } else {
+                int curr = prev + next;
+                System.out.println(curr);
+                prev = next;
+                next = curr;
+            }
+        }
+    }
+}
