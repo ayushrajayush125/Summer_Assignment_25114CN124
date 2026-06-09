fe52079b1fbe2b3927e90c486e786d6ebbb1e793
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-09 10:13:43 +0530

    Update Q1.java

--- DAY 5/Q1.java
+++ DAY 5/Q1.java
@@ -1,25 +1,19 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
+import java.util.*;
 
-int main(){
-    cout << "Write a program to check perfect number.\n";
-    int n =0;
-    cout << "Enter N: ";
-    cin >> n;
-    int sum =0;
-    if(n<=1) cout<<"invalid input";
-
-    for (int i = 1; i < n; i++)
-    {
-        if(n%i ==0){
-            sum+=i;
-        }
-    }
-    if(sum==n){
-        cout<<"true";
-    }else{
-        cout<<"false";
-    }
-    return 0;
+public class Q1{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Write a program to check perfect number.");
+int n=sc.nextInt();
+int sum=0;
+if(n<=1){
+System.out.print("invalid input");
+return;
+}
+for(int i=1;i<n;i++){
+if(n%i==0)sum+=i;
+}
+if(sum==n)System.out.print("true");
+else System.out.print("false");
+}
 }
