ef9ded490be8c2139f06271a64360f0dff483ba9
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-11 16:59:04 +0530

    Add files via upload

---
+++ DAY 5/Q3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args){
+        System.out.println("Write a program to Print factors of a number.");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n=sc.nextInt();
+
+        if(n<0){
+            System.out.print("invalid input");
+        }else if(n==1){
+            System.out.print(1);
+        }else if(n==0){
+            System.out.print("every non-zero number is a factor.");
+        }else{
+            for(int i=1;i<=n;i++){
+                if(n%i==0){
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
