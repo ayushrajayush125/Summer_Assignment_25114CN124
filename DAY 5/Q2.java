e44ffc9bc27a7aac98bf82a4506821eb2dbd5102
ayushrajayush125 <25114cn124@glbitm.ac.in>
2026-06-11 16:42:06 +0530

    Add strong number check program in Q2.java
    Implement a program to check if a number is strong by calculating the factorial of its digits and comparing the sum to the original number.

---
+++ DAY 5/Q2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main{
+    static int getFac(int i){
+        if(i==0){
+            return 1;
+        }
+        return i*getFac(i-1);
+    }
+
+    public static void main(String[]args){
+        System.out.println("Write a program to check strong number.");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n=sc.nextInt();
+        int sum=0;
+        int x=n;
+
+        while(x>0){
+            sum+=getFac(x%10);
+            x/=10;
+        }
+
+        if(sum==n){
+            System.out.print("true");
+        }else{
+            System.out.print("false");
+        }
+    }
+}
